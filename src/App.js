270f597b49569d6b75610d1b5bb44d0b2e35e59f
Adisa Ibrahim Oluwabukola <ibrahimadisa99@gmail.com>
2024-05-03 13:48:26 +0530

    React project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+function App(){
+  return(
+    <>
+    <h1>Ibrahim</h1>
+    <h1>Swastik</h1>
+    <Navbar/>
+    </>
+  )
+}
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return ( 
+        <>
+            <h1>Hello everyone!</h1>
+        </>
+     );
+}
+
+export default Navbar;
